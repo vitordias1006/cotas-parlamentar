ce187584d8c9778fb6dc0ee504be4ae7bb8fea2e
vitordias1006 <vitordias09532@gmail.com>
2026-03-23 15:16:21 -0300

    CRUD Manter Cota Básica atualizado

--- src/main/java/br/com/indicecotabasica/cotasparlamentar/controllers/IndiceController.java
+++ src/main/java/br/com/indicecotabasica/cotasparlamentar/controllers/IndiceController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/indices")
 @RequiredArgsConstructor
--- src/main/java/br/com/indicecotabasica/cotasparlamentar/handlers/IndiceRequestException.java
+++
@@ -1,7 +0,0 @@
-package br.com.indicecotabasica.cotasparlamentar.handlers;
-
-public class IndiceRequestException extends RuntimeException {
-    public IndiceRequestException(String message) {
-        super(message);
-    }
-}
